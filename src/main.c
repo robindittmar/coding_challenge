68d313f0e67d3df5e0d682aa661dec172a959bed
Robin Dittmar <robindittmar@gmail.com>
2025-06-24 15:50:28 +0200

    added gvm-libs to cmake target

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <gvm/util/jsonpull.h>
+
 
 int main(void) {
-    printf("Hello, World!\n");
+    FILE* fp = fopen("messages.json", "r");
+    if (!fp) {
+        return -1;
+    }
+
+    gvm_json_pull_parser_t parser;
+    gvm_json_pull_parser_init(&parser, fp);
+    // TBD
+    gvm_json_pull_parser_cleanup(&parser);
+
+    fclose(fp);
     return 0;
 }
